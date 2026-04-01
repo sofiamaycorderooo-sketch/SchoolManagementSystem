50b150d2d60b65d498451def5f0042b05036abd0
sofiamaycorderooo-sketch <sofiamaycorderooo@gmail.com>
2026-04-01 09:35:02 +0800

    Update LoginForm.java
    Improved Login Form UI

--- LoginForm.java
+++ LoginForm.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginForm extends JFrame {
+    // Improved login UI
     private JTextField userField;
     private JPasswordField passField;
     private JButton loginBtn;
